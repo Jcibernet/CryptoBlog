5a3b4dcf4f856bea86eff0e71cfb75d020550284
juan <campiasjuan@gmail.com>
2022-02-08 14:53:59 -0300

    implemented link_to_post

--- theblog/views.py
+++ theblog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
 
-    return render(request, 'home.html', {})
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
