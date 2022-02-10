cbc79046502b3626b6504b51cc67783f83688de6
juan <campiasjuan@gmail.com>
2022-02-10 16:15:05 -0300

    Added templates and Views

--- theblog/views.py
+++ theblog/views.py
@@ -7,6 +7,8 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    #ordering = ['-id']
+    #'id' del post
 
 class ArticleDetailView(DetailView):
     model = Post
